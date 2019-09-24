439838b11fc0a02d96b8cbc42ed48be8596f6f7f
root <adrianligad@yahoo.com>
2019-09-23 18:53:56 -0700

    got relic and artillery complete, working server and module, ready to push to ec2

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -1,12 +1,15 @@
+require('newrelic');
 const express = require('express');
-
 const app = express();
 const bodyParser = require('body-parser');
-
+const cors = require ('cors')
 const port = 3000;
+app.use(cors())
 
 
-app.use('/:id/', express.static('public'));
+
+app.use('/:id', express.static('public'));
+
 
 
 app.listen(port, () => { console.log(`Proxy server listening on port ${port}`); });
